24c2463d5facf52d2db9d3654847dca8d90af488
DJSardorbek <githubsardorbek65119@gmail.com>
2023-01-31 03:59:48 +0500

    style

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '@/styles/globals.scss'
-import 'tailwindcss/tailwind.css'
+import '@/styles/globals.scss';
+import 'tailwindcss/tailwind.css';
 import {Layout} from "@/Components";
 export default function App({ Component, pageProps }) {
   return (
